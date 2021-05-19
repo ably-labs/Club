1e8b5a4c9d4c593f8040032c101f51d09f556f1e
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-05-19 21:34:13 +0100

    Improve username change modal

--- web/public/ts/ui/EditUsernameModal.tsx
+++ web/public/ts/ui/EditUsernameModal.tsx
@@ -1,8 +1,7 @@
 import React, {useRef, useState} from 'react';
 import {Dialog} from "@headlessui/react";
 import {generateRandomUsername} from "../names";
-import {FaTimes} from "react-icons/fa";
-import {pickRandomTailwindColor} from "../colors";
+import {FaRedo, FaTimes} from "react-icons/fa";
 
 interface Props {
     handleSubmit: (string) => void
@@ -13,7 +12,7 @@ interface Props {
 export default function EditUsernameModal({handleSubmit, show, handleClose}: Props) {
     const usernameFieldRef = useRef(null)
     const [usernameField, setUsernameField] = useState("")
-    const [randomColor] = useState(pickRandomTailwindColor())
+    const [randomName] = useState(generateRandomUsername())
 
     if (!show) {
         return <></>
@@ -30,24 +29,29 @@ export default function EditUsernameModal({handleSubmit, show, handleClose}: Pro
     return (
         <Dialog
             initialFocus={usernameFieldRef} open={show} onClose={handleClose}
-            className={"fixed inset-0 border-2 overflow-y-auto z-10"}>
-            <div
-                className={`flex items-center justify-center min-h-screen flex-col`}>
+            className={"fixed inset-0 border-2 overflow-y-auto z-10 bg-red-900 text-red-800"}>
+            <button className={"absolute right-6 top-6 hover:opacity-50"} onClick={handleClose}>
+                <FaTimes size={48}/>
+            </button>
+            <div className={`flex min-h-screen flex-col inline-block`}>
                 {/*<Dialog.Overlay className={"fixed inset-0 bg-white opacity-70"}/>*/}
-                <button className={"inset-0"} onClick={handleClose}><FaTimes size={48}/></button>
-                <Dialog.Title className={"text-2xl"}>Edit username</Dialog.Title>
-                <Dialog.Description>Others will see a new name</Dialog.Description>
-                <label className="text-2xl" htmlFor={"usernameField"}>New username:</label>
-                <div className={"flex"}>
-                    <input ref={usernameFieldRef} className={"text-2xl text-white-800"} value={usernameField}
-                           onChange={onChangeUsernameField} name={"usernameField"} enterKeyHint={"enter"}
-                           placeholder={"Bald Eagle"}
-                           aria-label={"Enter a username..."} required={true}/>
-                    <button onClick={randomlyGenerateUsernameHandler}>Generate</button>
+                <div className={"min-w-800 bg-red-100 rounded-xl p-3 mx-auto my-auto"}>
+                    <label className={"my-3"} htmlFor={"usernameField"}>Enter a new username:</label>
+                    <div className={"flex my-3"}>
+                        <input ref={usernameFieldRef} className={"text-2xl text-white-800 border-full"}
+                               value={usernameField}
+                               onChange={onChangeUsernameField} name={"usernameField"} enterKeyHint={"enter"}
+                               placeholder={randomName}
+                               aria-label={"Enter a username..."} required={true}/>
+                        <button className={"m-3 hover:opacity-50"} onClick={randomlyGenerateUsernameHandler}><FaRedo/></button>
+                    </div>
+                    <div className={"flex justify-items-end"}>
+                        <button
+                            className={"bg-green-500 hover:bg-green-700 text-white mx-2 font-bold py-2 px-4 rounded-full"}
+                            onClick={() => handleSubmit(usernameField)}>Save
+                        </button>
+                    </div>
                 </div>
-                <button className={"bg-green-500 hover:bg-green-700 text-white mx-2 font-bold py-2 px-4 rounded"}
-                        onClick={() => handleSubmit(usernameField)}>Save
-                </button>
             </div>
         </Dialog>
     );

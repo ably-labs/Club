b524595a478201cb9f4340b24c4a72d535a6f69d
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-08-26 17:30:44 +0100

    Update mediapipe holistic to use memory leak fix

--- web/public/ts/ui/TextGuide.tsx
+++ web/public/ts/ui/TextGuide.tsx
@@ -14,12 +14,6 @@ const TextGuide = (): React.ReactElement => {
             <p className={"text-2xl font-bold"}>Mobile Support</p>
             <span>{"Not currently supported because the Mediapipe library which provides the face mesh doesn't run outside of Desktop Chrome/ Brave."}</span>
         </div>
-        <div className={"mt-6"}>
-            <p className={"text-2xl font-bold"}>Memory leak alert</p>
-            <span>There is a memory leak in this app, so it gradually takes up 2GB of memory over the course of 10 minutes, and then freezes the app. This is due to MediaPipe not cleaning up its arrays in WASM, see this <a
-                className={"hover:underline text-blue-600"}
-                href={"https://github.com/google/mediapipe/issues/1937"}>GitHub issue</a> for more.</span>
-        </div>
     </div>;
 }
 
